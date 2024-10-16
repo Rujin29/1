3b495a1bfd373b8c3d9e72e422d75c0cad19f49e
Rujin29 <rostyslav.storchak@gmail.com>
2024-10-16 12:04:06 +0300

    New file was added to the repo

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import json
 import requests
+import matplotlib.pyplot as plt
 
+# Запрос к API НБУ
 url_string = "https://bank.gov.ua/NBU_Exchange/exchange_site?json&start=20241007&end=20241011&valcode=usd"
+my_response = requests.get(url_string)
 
-my_responce = requests.get(url_string)
+# Проверка статуса запроса
+if my_response.status_code == 200:
+    response_json = json.loads(my_response.content)
 
-print(my_responce)
-print(my_responce.content)
+    # Сбор дат и курсов
+    dates = []
+    rates = []
 
-responce_json = json.loads(my_responce.content)
+    for item in response_json:
+        dates.append(item['exchangedate'])
+        rates.append(item['rate'])
 
-print(responce_json)
+    # Построение графика
+    plt.figure(figsize=(10, 5))
+    plt.plot(dates, rates, marker='o', linestyle='-', color='b')
 
-for item in responce_json:
-    print(item['rate'])
+    # Оформление графика
+    plt.title('Exchange Rate of USD (NBU)', fontsize=14)
+    plt.xlabel('Date', fontsize=12)
+    plt.ylabel('Exchange Rate (UAH)', fontsize=12)
+    plt.xticks(rotation=45)  # Поворот подписей по оси X для лучшей читаемости
+    plt.grid(True)
+
+    # Показ графика
+    plt.tight_layout()
+    plt.show()
+else:
+    print("Error: Unable to fetch data")
+
 
