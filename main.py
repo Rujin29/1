e282d8d77f6065190c8a0cf36bc9baa55cda20d4
Rujin29 <rostyslav.storchak@gmail.com>
2024-10-16 12:01:20 +0300

    New file was added to the repo

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import json
 import requests
-
-from urllib3 import request
 
 url_string = "https://bank.gov.ua/NBU_Exchange/exchange_site?json&start=20241007&end=20241011&valcode=usd"
 
-requests.get(url_string)
+my_responce = requests.get(url_string)
+
+print(my_responce)
+print(my_responce.content)
+
+responce_json = json.loads(my_responce.content)
+
+print(responce_json)
+
+for item in responce_json:
+    print(item['rate'])
+
